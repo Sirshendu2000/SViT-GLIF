20e2364872936ddde6237827f0d944beb2e3e0c9
Sirshendu <das.sirshendu.2000@gmail.com>
2025-03-31 13:06:09 +0530

    Models for CIFAR100

---
+++ cifar100/SViT_GLIF.py
@@ -0,0 +1,300 @@
+import torch
+import torch.nn as nn
+from spikingjelly.clock_driven.neuron import  MultiStepLIFNode, BaseNode
+from timm.models.layers import to_2tuple, trunc_normal_, DropPath
+from timm.models.registry import register_model
+from timm.models.vision_transformer import _cfg
+import torch.nn.functional as F
+from functools import partial
+import math
+from my_spiking.my_spikingjelly.activation_based.neuron import GaussianIntegrateAndFire
+import numpy as np
+
+def get_beta_schedule(num_diffusion_steps, name="cosine"):
+    betas = []
+    if name == "cosine":
+        max_beta = 0.999
+        f = lambda t: np.cos((t + 0.008) / 1.008 * np.pi / 2) ** 2
+        for i in range(num_diffusion_steps):
+            t1 = i / num_diffusion_steps
+            t2 = (i + 1) / num_diffusion_steps
+            betas.append(min(1 - f(t2) / f(t1), max_beta))
+        betas = np.array(betas, dtype=np.float32)
+    elif name == "linear":
+        scale = 1000 / num_diffusion_steps
+        beta_start = scale * 0.0001
+        beta_end = scale * 0.02
+        betas = np.linspace(beta_start, beta_end, num_diffusion_steps, dtype=np.float32)
+    else:
+        raise NotImplementedError(f"unknown beta schedule: {name}")
+    return betas
+
+class MLP(nn.Module):
+    def __init__(self, in_features, hidden_features=None, out_features=None, drop=0., beta=[0.25, 0.44, 0.64, 0.99]):
+        super().__init__()
+        self.beta=beta
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        self.fc1_linear = nn.Linear(in_features, hidden_features)
+        self.fc1_bn = nn.BatchNorm1d(hidden_features)
+        self.fc1_lif =  GaussianIntegrateAndFire(tau=2.0, detach_reset=True )
+
+        self.fc2_linear = nn.Linear(hidden_features, out_features)
+        self.fc2_bn = nn.BatchNorm1d(out_features)
+        self.fc2_lif =  GaussianIntegrateAndFire(tau=2.0, detach_reset=True )
+
+        self.c_hidden = hidden_features
+        self.c_output = out_features
+
+    def forward(self, x):
+        T,B,N,C = x.shape
+        x= x.to(torch.float32)
+        x_ = x.flatten(0, 1)
+        x = self.fc1_linear(x_)
+        x = self.fc1_bn(x.transpose(-1, -2)).transpose(-1, -2).reshape(T, B, N, self.c_hidden).contiguous()
+        x = self.fc1_lif(x)
+        # x=x.to(torch.float32)
+
+        x = self.fc2_linear(x.flatten(0,1))
+        x = self.fc2_bn(x.transpose(-1, -2)).transpose(-1, -2).reshape(T, B, N, C).contiguous()
+        x = self.fc2_lif(x)
+        # x=x.to(torch.float32)
+        return x
+
+
+class SSA(nn.Module):
+    def __init__(self, dim, num_heads=8, qkv_bias=False, qk_scale=None, attn_drop=0., proj_drop=0., sr_ratio=1, beta=[0.25, 0.44, 0.64, 0.99]):
+        super().__init__()
+        assert dim % num_heads == 0, f"dim {dim} should be divided by num_heads {num_heads}."
+        self.dim = dim
+        self.num_heads = num_heads
+        self.scale = 0.125
+        self.q_linear = nn.Linear(dim, dim)
+        self.q_bn = nn.BatchNorm1d(dim)
+        self.beta=beta
+        self.q_lif = GaussianIntegrateAndFire(tau=2.0, detach_reset=True, beta=self.beta)
+
+        self.k_linear = nn.Linear(dim, dim)
+        self.k_bn = nn.BatchNorm1d(dim)
+        self.k_lif = GaussianIntegrateAndFire(tau=2.0, detach_reset=True, beta=self.beta)
+
+        self.v_linear = nn.Linear(dim, dim)
+        self.v_bn = nn.BatchNorm1d(dim)
+        self.v_lif = GaussianIntegrateAndFire(tau=2.0, detach_reset=True, beta=self.beta)
+        self.attn_lif = GaussianIntegrateAndFire(tau=2.0, v_threshold=0.5, detach_reset=True, beta=self.beta)
+
+        self.proj_linear = nn.Linear(dim, dim)
+        self.proj_bn = nn.BatchNorm1d(dim)
+        self.proj_lif = GaussianIntegrateAndFire(tau=2.0, detach_reset=True, beta=self.beta)
+
+    def forward(self, x):
+        T,B,N,C = x.shape
+
+        x_for_qkv = x.flatten(0, 1)  # TB, N, C
+        q_linear_out = self.q_linear(x_for_qkv)  # [TB, N, C]
+        q_linear_out = self.q_bn(q_linear_out. transpose(-1, -2)).transpose(-1, -2).reshape(T, B, N, C).contiguous()
+        # print("q_linear_out",q_linear_out.shape)
+        q_linear_out = self.q_lif(q_linear_out)
+        # print("q_linear_out",q_linear_out.dtype)
+        q = q_linear_out.reshape(T, B, N, self.num_heads, C//self.num_heads).permute(0, 1, 3, 2, 4).contiguous()
+
+        k_linear_out = self.k_linear(x_for_qkv)
+        k_linear_out = self.k_bn(k_linear_out. transpose(-1, -2)).transpose(-1, -2).reshape(T, B, N, C).contiguous()
+        k_linear_out = self.k_lif(k_linear_out)
+        k = k_linear_out.reshape(T, B, N, self.num_heads, C//self.num_heads).permute(0, 1, 3, 2, 4).contiguous()
+
+        v_linear_out = self.v_linear(x_for_qkv)
+        v_linear_out = self.v_bn(v_linear_out. transpose(-1, -2)).transpose(-1, -2).reshape(T, B, N, C).contiguous()
+        v_linear_out = self.v_lif(v_linear_out)
+        v = v_linear_out.reshape(T, B, N, self.num_heads, C//self.num_heads).permute(0, 1, 3, 2, 4).contiguous()
+
+        attn = (q @ k.transpose(-2, -1)) * self.scale
+        x = attn @ v
+        x = x.transpose(2, 3).reshape(T, B, N, C).contiguous()
+        # print("x",x.shape)
+        x = self.attn_lif(x)
+        x = x.flatten(0, 1)
+        # x=x.to(torch.float32)
+        x = self.proj_lif(self.proj_bn(self.proj_linear(x).transpose(-1, -2)).transpose(-1, -2).reshape(T, B, N, C))
+        return x, attn
+
+class Block(nn.Module):
+    def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop=0., attn_drop=0.,
+                 drop_path=0., norm_layer=nn.LayerNorm, sr_ratio=1, beta=[0.25, 0.44, 0.64, 0.99]):
+        super().__init__()
+        self.norm1 = norm_layer(dim)
+        self.beta=beta
+        self.attn = SSA(dim, num_heads=num_heads, qkv_bias=qkv_bias, qk_scale=qk_scale,
+                              attn_drop=attn_drop, proj_drop=drop, sr_ratio=sr_ratio, beta=self.beta)
+        self.norm2 = norm_layer(dim)
+        mlp_hidden_dim = int(dim * mlp_ratio)
+        self.mlp = MLP(in_features=dim, hidden_features=mlp_hidden_dim, drop=drop, beta=self.beta)
+
+    def forward(self, x):
+        # x=x.to(torch.float32)
+        x_attn, attn = (self.attn(x))
+        x = x + x_attn
+        # x = x + self.attn(x)
+        x = x + self.mlp(x)
+        return x, attn
+
+
+class SPS(nn.Module):
+    def __init__(self, img_size_h=128, img_size_w=128, patch_size=4, in_channels=2, embed_dims=256, beta=[0.25, 0.44, 0.64, 0.99]):
+        super().__init__()
+        self.image_size = [img_size_h, img_size_w]
+        patch_size = to_2tuple(patch_size)
+        self.patch_size = patch_size
+        self.C = in_channels
+        self.H, self.W = self.image_size[0] // patch_size[0], self.image_size[1] // patch_size[1]
+        self.num_patches = self.H * self.W
+        self.beta=beta
+        self.proj_conv = nn.Conv2d(in_channels, embed_dims//8, kernel_size=3, stride=1, padding=1, bias=False)
+        self.proj_bn = nn.BatchNorm2d(embed_dims//8)
+        self.proj_lif =  GaussianIntegrateAndFire(tau=2.0, detach_reset=True )
+
+        self.proj_conv1 = nn.Conv2d(embed_dims//8, embed_dims//4, kernel_size=3, stride=1, padding=1, bias=False)
+        self.proj_bn1 = nn.BatchNorm2d(embed_dims//4)
+        self.proj_lif1 =  GaussianIntegrateAndFire(tau=2.0, detach_reset=True )
+
+        self.proj_conv2 = nn.Conv2d(embed_dims//4, embed_dims//2, kernel_size=3, stride=1, padding=1, bias=False)
+        self.proj_bn2 = nn.BatchNorm2d(embed_dims//2)
+        self.proj_lif2 =  GaussianIntegrateAndFire(tau=2.0, detach_reset=True )
+        self.maxpool2 = torch.nn.MaxPool2d(kernel_size=3, stride=2, padding=1, dilation=1, ceil_mode=False)
+
+        self.proj_conv3 = nn.Conv2d(embed_dims//2, embed_dims, kernel_size=3, stride=1, padding=1, bias=False)
+        self.proj_bn3 = nn.BatchNorm2d(embed_dims)
+        self.proj_lif3 =  GaussianIntegrateAndFire(tau=2.0, detach_reset=True )
+        self.maxpool3 = torch.nn.MaxPool2d(kernel_size=3, stride=2, padding=1, dilation=1, ceil_mode=False)
+
+        self.rpe_conv = nn.Conv2d(embed_dims, embed_dims, kernel_size=3, stride=1, padding=1, bias=False)
+        self.rpe_bn = nn.BatchNorm2d(embed_dims)
+        self.rpe_lif =  GaussianIntegrateAndFire(tau=2.0, detach_reset=True )
+
+    def forward(self, x):
+        T, B, C, H, W = x.shape
+        x = self.proj_conv(x.flatten(0, 1)) # have some fire value
+        x = self.proj_bn(x).reshape(T, B, -1, H, W).contiguous()
+        x = self.proj_lif(x).flatten(0, 1).contiguous()
+        # x=x.to(torch.float32)
+
+        # print("GIFFFFFFFFFFFFFFFFFF")
+        # print(x.shape)
+        # print(x.dtype)
+        # print(x)
+
+        x = self.proj_conv1(x)
+        x = self.proj_bn1(x).reshape(T, B, -1, H, W).contiguous()
+        x = self.proj_lif1(x).flatten(0, 1).contiguous()
+        # x=x.to(torch.float32)
+
+        x = self.proj_conv2(x)
+        x = self.proj_bn2(x).reshape(T, B, -1, H, W).contiguous()
+        x = self.proj_lif2(x).flatten(0, 1).contiguous()
+        # x=x.to(torch.float32)
+        x = self.maxpool2(x)
+
+
+        x = self.proj_conv3(x)
+        x = self.proj_bn3(x).reshape(T, B, -1, H//2, W//2).contiguous()
+        x = self.proj_lif3(x).flatten(0, 1).contiguous()
+        # x=x.to(torch.float32)
+        x = self.maxpool3(x)
+
+        x_feat = x.reshape(T, B, -1, H//4, W//4).contiguous()
+        x = self.rpe_conv(x)
+        x = self.rpe_bn(x).reshape(T, B, -1, H//4, W//4).contiguous()
+        x = self.rpe_lif(x)
+        # x=x.to(torch.float32)
+        x = x + x_feat
+
+        x = x.flatten(-2).transpose(-1, -2)  # T,B,N,C
+        return x
+
+
+class Spikformer(nn.Module):
+    def __init__(self,
+                 img_size_h=128, img_size_w=128, patch_size=16, in_channels=2, num_classes=11,
+                 embed_dims=[64, 128, 256], num_heads=[1, 2, 4], mlp_ratios=[4, 4, 4], qkv_bias=False, qk_scale=None,
+                 drop_rate=0., attn_drop_rate=0., drop_path_rate=0., norm_layer=nn.LayerNorm,
+                 depths=[6, 8, 6], sr_ratios=[8, 4, 2], T = 4, beta=0.25
+                 ):
+        super().__init__()
+        self.T = T  # time step
+        self.num_classes = num_classes
+        self.depths = depths
+        # self.beta=get_beta_schedule(num_diffusion_steps=self.T, name='cosine')      #  ??????????????????????????????????????????????????????????????
+        self.beta=beta
+        print("beta", self.beta)
+
+        dpr = [x.item() for x in torch.linspace(0, drop_path_rate, depths)]  # stochastic depth decay rule
+
+        patch_embed = SPS(img_size_h=img_size_h,
+                                 img_size_w=img_size_w,
+                                 patch_size=patch_size,
+                                 in_channels=in_channels,
+                                 embed_dims=embed_dims,
+                                 beta=self.beta)
+
+        block = nn.ModuleList([Block(
+            dim=embed_dims, num_heads=num_heads, mlp_ratio=mlp_ratios, qkv_bias=qkv_bias,
+            qk_scale=qk_scale, drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[j],
+            norm_layer=norm_layer, sr_ratio=sr_ratios, beta=self.beta)
+            for j in range(depths)])
+
+        setattr(self, f"patch_embed", patch_embed)
+        setattr(self, f"block", block)
+
+        # classification head
+        self.head = nn.Linear(embed_dims, num_classes) if num_classes > 0 else nn.Identity()
+        self.apply(self._init_weights)
+
+    @torch.jit.ignore
+    def _get_pos_embed(self, pos_embed, patch_embed, H, W):
+        if H * W == self.patch_embed1.num_patches:
+            return pos_embed
+        else:
+            return F.interpolate(
+                pos_embed.reshape(1, patch_embed.H, patch_embed.W, -1).permute(0, 3, 1, 2),
+                size=(H, W), mode="bilinear").reshape(1, -1, H * W).permute(0, 2, 1)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            trunc_normal_(m.weight, std=.02)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+
+    def forward_features(self, x):
+
+        block = getattr(self, f"block")
+        patch_embed = getattr(self, f"patch_embed")
+
+        x = patch_embed(x)
+        # print("EMBEDDING & ENCODING DONE!!!!!!")
+        for blk in block:
+            x, attn = blk(x)
+        return x.mean(2), attn
+
+    def forward(self, x):
+        x = (x.unsqueeze(0)).repeat(self.T, 1, 1, 1, 1)
+        # print(x.dtype)
+        x, attn = self.forward_features(x)
+        # x=x.to(torch.float32)
+        x = self.head(x.mean(0))
+        return x
+
+
+@register_model
+def spikformer_with_gif(pretrained=False, **kwargs):
+    model = Spikformer(
+        # img_size_h=224, img_size_w=224,
+        # patch_size=16, embed_dims=768, num_heads=12, mlp_ratios=4,
+        # in_channels=3, num_classes=1000, qkv_bias=False,
+        # norm_layer=partial(nn.LayerNorm, eps=1e-6), depths=12, sr_ratios=1,
+        **kwargs
+    )
+    model.default_cfg = _cfg()
+    return model
